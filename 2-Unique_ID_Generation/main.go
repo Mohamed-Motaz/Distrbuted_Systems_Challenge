952187a94dfce0454d4d020a2936b7e85bcd8466
Mohamed ElZein <mohamed.elzien@careem.com>
2024-01-07 17:30:27 +0200

    finish second challenge

---
+++ 2-Unique_ID_Generation/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"sync"
+	"sync/atomic"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+// go install . && ~/Downloads/maelstrom/maelstrom test -w unique-ids --bin ~/go/bin/maelstrom-unique-ids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+
+//Since we have a known number of nodes, we can simply allocate of chunk of ids that each
+//node is allowed to return
+
+var UNIQUE_ID atomic.Int64 = atomic.Int64{}
+var once sync.Once = sync.Once{}
+
+// Each node has a different start range
+// assumption is that number of ids per node will never be exhausted
+// and will therefore always be valid
+func Init(n *maelstrom.Node) {
+	id, _ := strconv.Atoi(n.ID()[1:])
+	start := math.MaxInt64 * (float64(id) / float64(len(n.NodeIDs())))
+	fmt.Fprintf(os.Stderr, "Starts for node %+v is: %+v\n", n.ID(), int64(start))
+
+	UNIQUE_ID.Store(int64(start))
+
+	fmt.Fprintf(os.Stderr, "Final start: %+v\n", UNIQUE_ID.Load())
+}
+
+func main() {
+
+	n := maelstrom.NewNode()
+
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		once.Do(func() {
+			Init(n)
+		})
+
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type to return back.
+		body["type"] = "generate_ok"
+		body["id"] = fmt.Sprintf("%d", IncrAndGetId()) //have to stringify the id because json can't deal with large numbers
+
+		// Echo the original message back with the updated message type.
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+}
+
+func IncrAndGetId() int64 {
+	UNIQUE_ID.Add(1)
+	return UNIQUE_ID.Load()
+}
